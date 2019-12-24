7f8ae4e52306b0b804f66e921973a207b281d233
Parsa Hejabi <parsa.hejabi@gmail.com>
2019-12-24 03:36:41 +0330

    Fixed eslint errors on back-end.

--- back-end/index.js
+++ back-end/index.js
@@ -7,66 +7,67 @@ const logger = require('./logger');
 const app = express();
 const port = process.env.PORT || 9000;
 
-const form_descriptors_path = path.join(__dirname, 'form-descriptors');
+const FORM_DESCRIPTORS_PATH = path.join(__dirname, 'form-descriptors');
 
 console.log('Starting back-end application...');
 app.use(cors());
 app.use('/', (req, res, next) => {
-    logger.logger.info({
-        message: 'Request: ',
-        url: req.url,
-        method: req.method,
-        ip: req.ip,
-        query: req.query,
-    });
+  logger.logger.info({
+    message: 'Request: ',
+    url: req.url,
+    method: req.method,
+    ip: req.ip,
+    query: req.query,
+  });
 
-    next();
+  next();
 });
 
-app.use('/static', express.static(form_descriptors_path));
+app.use('/static', express.static(FORM_DESCRIPTORS_PATH));
 
 app.get('/api/forms', (req, res) => {
-    const list_of_form_descriptors = fs.readdirSync(form_descriptors_path);
-    const result = list_of_form_descriptors.map((item, index) => {
-        try {
-            let data = fs.readFileSync(path.join(form_descriptors_path, item));
-            let form_descriptor = JSON.parse(data);
-            return {
-                'id': form_descriptor.id,
-                'title': form_descriptor.title,
-                'url': req.url + form_descriptor.id
-            }
-        } catch (err) {
-            logger.logger.error('Error reading file from disk: ', err);
-        }
-    });
-    res.send(result);
+  const LIST_OF_FORM_DESCRIPTORS = fs.readdirSync(FORM_DESCRIPTORS_PATH);
+  const result = LIST_OF_FORM_DESCRIPTORS.map((item) => {
+    try {
+      const data = fs.readFileSync(path.join(FORM_DESCRIPTORS_PATH, item));
+      const FORM_DESCRIPTOR = JSON.parse(data);
+      return {
+        id: FORM_DESCRIPTOR.id,
+        title: FORM_DESCRIPTOR.title,
+        back_end_url: req.url + FORM_DESCRIPTOR.id,
+      };
+    } catch (err) {
+      logger.logger.error('Error reading file from disk: ', err);
+      return {};
+    }
+  });
+  res.send(result);
 });
 
 app.get('/api/forms/:formId(\\d+)', (req, res) => {
-    const formId = req.params.formId;
-    const list_of_form_descriptors = fs.readdirSync(form_descriptors_path);
-    let result = null;
-    list_of_form_descriptors.forEach((item, index) => {
-        try {
-            let data = fs.readFileSync(path.join(form_descriptors_path, item));    
-            let form_descriptor = JSON.parse(data);
-            if (form_descriptor.id === formId) {
-                result = form_descriptor;
-            }
-        } catch (err) {
-            logger.logger.error('Error reading file from disk: ', err);
-        }
-    });
-
-    if (result == null) {
-        res.status(404)
-            .json({
-                'err': 'Couldn\'t find the file you requested!'
-            });
-    } else {
-        res.json(result);
+  const { formId } = req.params;
+  const LIST_OF_FORM_DESCRIPTORS = fs.readdirSync(FORM_DESCRIPTORS_PATH);
+  let result = null;
+  LIST_OF_FORM_DESCRIPTORS.forEach((item) => {
+    try {
+      const data = fs.readFileSync(path.join(FORM_DESCRIPTORS_PATH, item));
+      const FORM_DESCRIPTOR = JSON.parse(data);
+      if (FORM_DESCRIPTOR.id === formId) {
+        result = FORM_DESCRIPTOR;
+      }
+    } catch (err) {
+      logger.logger.error('Error reading file from disk: ', err);
     }
+  });
+
+  if (result == null) {
+    res.status(404)
+      .json({
+        err: 'Couldn\'t find the file you requested!',
+      });
+  } else {
+    res.json(result);
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
